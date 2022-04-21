d8bf20e300d4c333d938d3feeabba9f610367701
Chris Moates <chris.moates@davita.com>
2022-04-21 14:16:27 -0400

    Add more useful outputs and HTTP test

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
 locals {
-  myvar = "fern"
+  location = "USA"
+  department = "Costuming"
+  ppsid = "1234567"
 }
 
-output "myvar" {
-  value = local.myvar
+data "http" "ip" {
+  url = "https://ifconfig.me"
+}
+
+output "location" {
+  value = local.location
+}
+
+output "department" {
+  value = local.department
+}
+
+output "ppsid" {
+  value = local.ppsid
+}
+
+output "ip" {
+  value data.http.ip
 }
